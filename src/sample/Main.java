a4438714b2d76f2c9a43c837a0215ba4252108da
chkaradimitriou <chkaradimitriou@gmail.com>
2021-04-16 17:52:48 +0300

    init project

---
+++ src/sample/Main.java
@@ -0,0 +1,34 @@
+package sample;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Diakrinousa diakrinousa = new Diakrinousa();
+//        diakrinousa.solveDiakrousa();
+//
+//        BankATM atm = new BankATM();
+//        atm.runBankAtm();
+
+//        CatLesson catLesson = new CatLesson();
+//        catLesson.createCats();
+
+//        RealityShowsLessons realityShowsLessons = new RealityShowsLessons();
+//        realityShowsLessons.createRealityShowsLessons();
+
+//        MovieLessons movies = new MovieLessons();
+//        movies.createMovieLessons();
+
+//        PowerRangers powerRanger = new PowerRangers();
+//        powerRanger.createList();
+
+//        DogLesson dogLesson = new DogLesson();
+//        dogLesson.createList();
+
+//        FiestaPlayersLesson fiestaPlayersLesson = new FiestaPlayersLesson();
+//        fiestaPlayersLesson.createList();
+
+        MyShopLesson myShopLesson = new MyShopLesson();
+        myShopLesson.createList();
+      }
+
+}
