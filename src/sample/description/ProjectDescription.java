3b8d8279742ce15e6f7974b2237930ac73c293da
chkaradimitriou <chkaradimitriou@gmail.com>
2021-04-16 18:15:37 +0300

    Item model created

---
+++ src/sample/description/ProjectDescription.java
@@ -0,0 +1,8 @@
+package sample.description;
+
+public class ProjectDescription {
+    /*
+    Mixanismos pou pernei items : shopping list : katigories proionton : timh, posotita
+    9a ta exagei se ena txt arxeio
+     */
+}
