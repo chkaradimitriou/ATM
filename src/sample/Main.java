880599c136df96f179e035c6f48e2cae6695f199
chkaradimitriou <chkaradimitriou@gmail.com>
2021-04-18 19:26:43 +0300

    Checkpoint

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,33 @@
 package sample;
 
-import sample.logic.ShoppingList;
+import sample.model.Item;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ShoppingList shoppingList = new ShoppingList();
-        shoppingList.createList();
-      }
+        getItemsFromKeyboard();
+    }
 
+    private static void getItemsFromKeyboard(){
+        System.out.println("Enter Item Name");
+        Scanner nameScanner = new Scanner(System.in);
+        String itemName = nameScanner.nextLine();
+
+        System.out.println("Enter Item Price");
+        Scanner priceScanner = new Scanner(System.in);
+        Double price = priceScanner.nextDouble();
+
+        System.out.println("Enter Item Quantity");
+        Scanner quantityScanner = new Scanner(System.in);
+        Integer quantity = quantityScanner.nextInt();
+
+        createItem(itemName, price, quantity);
+    }
+
+    private static void createItem(String name, Double price, Integer quantity) {
+        Item item = new Item(name, price, quantity);
+        System.out.println("You bought:" + " " + item.getQuantity() + " x " + item.getName() + " for " + price+ "€");
+    }
 }
--- src/sample/logic/ShoppingList.java
+++ src/sample/logic/ShoppingList.java
@@ -5,20 +5,38 @@ import java.util.ArrayList;
 
 public class ShoppingList {
 
-    public void createList() {
-        ArrayList<Item> item = new ArrayList<>();
-        Item item1 = new Item("eggs",1.35, 6);
-        Item item2 = new Item("potatoes",0.79, 2);
-        Item item3 = new Item("CocaCola",1.20, 3);
-        Item item4 = new Item("cake",3.56, 2);
-        Item item5 = new Item("Milk",1.32, 4);
+    ArrayList<Item> item = new ArrayList<>();
+    Item item1;
+    Item item2;
+    Item item3;
+    Item item4;
+    Item item5;
 
+    public void createList() {
+        createListItems();
+
+        insertListItems();
+
+        printItemData();
+    }
+
+    private void createListItems(){
+        item1 = new Item("eggs",1.35, 6);
+        item2 = new Item("potatoes",0.79, 2);
+        item3 = new Item("CocaCola",1.20, 3);
+        item4 = new Item("cake",3.56, 2);
+        item5 = new Item("Milk",1.32, 4);
+    }
+
+    private void insertListItems() {
         item.add(item1);
         item.add(item2);
         item.add(item3);
         item.add(item4);
         item.add(item5);
+    }
 
+    private void printItemData() {
         for (int i=0; i< item.size(); i++){
             Double price = item.get(i).getPrice();
             Integer quantity = item.get(i).getQuantity();
