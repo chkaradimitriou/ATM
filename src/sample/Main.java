2503c4abdfe6a2deb2c3510ee426514a1eea528a
chkaradimitriou <chkaradimitriou@gmail.com>
2021-04-18 19:47:18 +0300

    write to txt file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,15 +2,19 @@ package sample;
 
 import sample.model.Item;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         getItemsFromKeyboard();
     }
 
-    private static void getItemsFromKeyboard(){
+    private static void getItemsFromKeyboard()  {
         System.out.println("Enter Item Name");
         Scanner nameScanner = new Scanner(System.in);
         String itemName = nameScanner.nextLine();
@@ -29,5 +33,23 @@ public class Main {
     private static void createItem(String name, Double price, Integer quantity) {
         Item item = new Item(name, price, quantity);
         System.out.println("You bought:" + " " + item.getQuantity() + " x " + item.getName() + " for " + price+ "€");
+
+        File file1 = new File("out.txt");
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw);
+
+
+        pw.println(name);
+        pw.println(String.valueOf(price));
+        pw.println(quantity);
+
+        pw.close();
+
+
     }
 }
